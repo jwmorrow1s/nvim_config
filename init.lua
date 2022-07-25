9ca7981a97c448e4d63e01ee0d333508d7a9c500
Jeff Morrow <jwmorrow1s@gmail.com>
2022-07-24 22:43:13 -0500

    initial

---
+++ init.lua
@@ -0,0 +1,16 @@
+require('plugins')
+require('lsp')
+require('treesitter')
+
+-- formatoptions-=ro not working
+        
+vim.api.nvim_exec([[
+    set number "set line numbers
+    autocmd FileType * setlocal formatoptions-=r formatoptions-=o "disable newline comments. Very annoying
+    set expandtab     " these two settings change tabs to 4 spaces 
+    set shiftwidth=4  " these two settings change tabs to 4 spaces 
+    colorscheme gruvbox-baby
+]], true)
+
+-- Able to diagnose in buffer with :LspLog
+vim.lsp.set_log_level("debug")
