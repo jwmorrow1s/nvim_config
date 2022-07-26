c6d39de1e6e1ab1c35a1dd7cefe3b484089b35c0
Jeff Morrow <jwmorrow1s@gmail.com>
2022-07-26 17:38:04 -0500

    updated completions

--- lua/completions.lua
+++ lua/completions.lua
@@ -2,18 +2,61 @@ local lspkind = require "lspkind"
 lspkind.init()
 
 local cmp = require("cmp")
-
 cmp.setup {
   mapping = {
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-u>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<c-y>"] = cmp.mapping.confirm{
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
+    ["<c-y>"] = cmp.mapping(
+      cmp.mapping.confirm{
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+        {'i', 'c'}
+      }),
     ["<c-space>"] = cmp.mapping.complete{},
 
+  },
+  ["<c-space>"] = cmp.mapping {
+  i = cmp.mapping.complete{},
+  c = function(
+    _ --[[fallback]]
+  )
+    if cmp.visible() then
+      if not cmp.confirm { select = true } then
+        return
+      end
+    else
+      cmp.complete()
+    end
+  end,
+  },
+  ["<tab>"] = cmp.config.disable,
+
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find "^_+"
+        local _, entry2_under = entry2.completion_item.label:find "^_+"
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
   -- sources are list in priority of how they try and get applied
   -- you can configure:
@@ -21,7 +64,6 @@ cmp.setup {
   --   priority
   --   max_item_count
   sources = {
-    { name = 'gh_issues' },
     { name = 'nvim_lua' },
     { name = 'path' },
     { name = 'luasnip' },
